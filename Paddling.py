6f98766d2ae1665b4d813eb66bfd39ee73b05f65
JohnScott <johnscott.jiang@gmail.com>
2023-07-07 01:20:34 +0800

    管他呢，我也不知道写了什么（格式化版）

--- Paddling.py
+++ Paddling.py
@@ -1,15 +1,18 @@
 from time import sleep
 
-def out(characters:str):
-    sleep(1)
-    print(characters,end='')
 
-def paddle(water:str):
+def out(characters: str):
+    sleep(1)
+    print(characters, end='')
+
+
+def paddle(water: str):
     if len(water) > 1:
         paddle(water[0:len(water)-1])
         paddle(water[1:len(water)])
     elif len(water) == 1:
         out(water)
+
 
 if __name__ == "__main__":
     paddle("What")
